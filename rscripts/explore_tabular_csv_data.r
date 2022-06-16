42729ce959ed9aee7de1fd616453449a6d35d374
Lucas Fortini <lfortini@usgs.gov>
2022-06-16 05:18:01 -1000

    added script that explores csv flat files files
    this has to be fine tuned, but it is a very easy way to get a first view of the data

---
+++ rscripts/explore_tabular_csv_data.r
@@ -0,0 +1,127 @@
+#this r file can be used to inspect tabular data in csv format to speed up data reviews 
+#Just specify the folder where the CSV files are and the script 
+#will generate a lot of info (including figures) on all columns for each csv included.
+#please note that the script will create diagnostic txt and tif files in the same folder where 
+#the csv files are located.
+
+##################
+#script config
+#only specify the directory where csvs are located. That is all.
+datarelease_dir="D:/projects/2021_ntbg/polbis_analysis/data/" 
+
+##################
+#under the hood
+setwd(datarelease_dir)
+csv_files=list.files(path=datarelease_dir, pattern=".csv$", recursive = T, full.names = F)
+
+sink.reset <- function(){
+  for(i in seq_len(sink.number())){
+    sink(NULL)
+  }
+}
+library(inspectdf)
+
+csv_file = csv_files[1]
+for (csv_file in csv_files){
+  cat("################\n")
+  cat(paste0("Doing file", csv_file, "\n"))
+  
+  #create a txt file with summary information for each CSV file
+  metadata_file=gsub(".csv$", "_1_metadata.txt", csv_file)
+  metadata_file_con=file(metadata_file, open="wt")
+  sink(metadata_file_con)
+  
+  cat("################\n")
+  cat("################\n")
+  cat("################\n")
+  cat(paste0("Doing file", csv_file, "\n"))
+  cat("################\n")
+
+  tmp_file=read.csv(csv_file)
+  cat("################\n")
+  cat("dimensions \n")
+  print(dim(tmp_file)) #shows the dimensions of the data frame by row and column
+  cat("################\n")
+  cat("Col names \n")
+  print(colnames(tmp_file)) #shows the name of each column in the data frame
+  
+  cat("################\n")
+  cat("Number of duplicated rows \n")
+  ndup=length(which(duplicated(tmp_file)))
+  print(ndup)
+  if (ndup>0) print(paste0("Duplicate rows ", which(duplicated(tmp_file)), "\n"))
+  
+  # cat("################\n")
+  # cat("structure \n")
+  # print(str(tmp_file)) #shows the structure of the data frame
+  
+  cat("################\n")
+  cat("summary \n")
+  print(summary(tmp_file)) #provides summary statistics on the columns of the data frame
+  
+  # cat("################\n")
+  # cat("Head \n")
+  # print(head(tmp_file)) #shows the first 6 rows of the data frame
+  # cat("################\n")
+  # cat("Tail \n")
+  # print(tail(tmp_file)) #shows the last 6 rows of the data frame
+  
+  # cat("################\n")
+  # cat("categorical columns \n")
+  # print(inspect_cat(tmp_file))
+  
+  # cat("################\n")
+  # cat("numeric correlations \n")
+  # print(inspect_cor(tmp_file))
+  
+  # cat("################\n")
+  # cat("memory profile \n")
+  # print(inspect_mem(tmp_file))
+  
+  cat("################\n")
+  cat("na occurrence \n")
+  print(inspect_na(tmp_file))
+  
+  cat("################\n")
+  cat("numeric summaries \n")
+  print(inspect_num(tmp_file))
+  
+  # cat("################\n")
+  # cat("data types \n")
+  # print(inspect_types(tmp_file))
+  
+  #close(metadata_file_con)
+  sink.reset()
+  ######################
+  ######################
+  #create some diagnostic graphics for each csv
+  noExt=gsub(".csv$", "", csv_file)
+  tiff(paste0(noExt, "_5_cat_plot.tif"), width = 15, height = 3, units = "in", pointsize = 12, compress="lzw", bg = "white", res = 300)
+  print(show_plot(inspect_cat(tmp_file)))
+  graphics.off()
+  
+  tiff(paste0(noExt, "_3_cor_plot.tif"), width = 12, height = 4, units = "in", pointsize = 12, compress="lzw", bg = "white", res = 300)
+  print(show_plot(inspect_cor(tmp_file)))
+  graphics.off()
+  
+  tiff(paste0(noExt, "_7_mem_plot.tif"), width = 8, height = 4, units = "in", pointsize = 12, compress="lzw", bg = "white", res = 300)
+  print(show_plot(inspect_mem(tmp_file)))
+  graphics.off()
+  
+  tiff(paste0(noExt, "_4_naFreq_plot.tif"), width = 8, height = 4, units = "in", pointsize = 12, compress="lzw", bg = "white", res = 300)
+  print(show_plot(inspect_na(tmp_file)))
+  graphics.off()
+  
+  tiff(paste0(noExt, "_2_numeric_plot.tif"), width = 12, height = 4, units = "in", pointsize = 12, compress="lzw", bg = "white", res = 300)
+  print(show_plot(inspect_num(tmp_file)))
+  graphics.off()
+  
+  tiff(paste0(noExt, "_6_dataTypes_plot.tif"), width = 8, height = 4, units = "in", pointsize = 12, compress="lzw", bg = "white", res = 300)
+  print(show_plot(inspect_types(tmp_file)))
+  graphics.off()
+  
+  #inspect_imb(tmp_file)
+  #stop sinks, close logs
+}
+
+
